46f5711edcab86ee6b7aed8e8e887558936a0f2d
apeckham <apeckham@gmail.com>
2015-07-02 14:51:39 +0200

    simplify test case

--- calculateshipping.php
+++ calculateshipping.php
@@ -9,109 +9,13 @@ function calculateshipping($DATA) {
 
 	/* do not edit above this line */
 
-	/*
-	NB: only the following php functions are allowed. Use of anything else will return a syntax error when you try to save.
-	'array_merge',
-	'array_diff',
-	'array_intersect',
-	'array_reverse',
-	'array_unique',
-	'in_array',
-	'date',
-	'range',
-	'geteparcelzonebypostcode',
-	'isset',
-	'ceil',
-	'floor',
-	'round',
-	'strtoupper',
-	'strtolower',
-	*/
-
-	//this holds the rates that will be returned to your customer
 	$_RATES = array();
 
-	/*
-	this is what $DATA looks like, you can use any of the info in $DATA to generate your shipping rate
-	Array
-	(
-		[origin] => Array
-			(
-				[country] => AU
-				[postal_code] => 3000
-				[province] => VIC
-				[city] => melbourne
-				[name] =>
-				[address1] => 1 main street
-				[address2] =>
-				[address3] =>
-				[phone] =>
-				[fax] =>
-				[address_type] =>
-				[company_name] =>
-			)
-
-		[destination] => Array
-			(
-				[country] => AU
-				[postal_code] => 2000
-				[province] => NSW
-				[city] =>
-				[name] =>
-				[address1] =>
-				[address2] =>
-				[address3] =>
-				[phone] =>
-				[fax] =>
-				[address_type] =>
-				[company_name] =>
-			)
-
-		[items] => Array
-			(
-				[0] => Array
-					(
-						[name] => product10
-						[sku] => SKUP00310
-						[quantity] => 1
-						[grams] => 1000
-						[price] => 300
-						[vendor] => PRODUCT
-						[requires_shipping] => 1
-						[taxable] => 1
-						[fulfillment_service] => manual
-						[product_id] => 128436738
-						[variant_id] => 290813760
-					)
-
-				[1] => Array
-					(
-						[name] => product11
-						[sku] => SKUP0011
-						[quantity] => 1
-						[grams] => 1100
-						[price] => 300
-						[vendor] => PRODUCT
-						[requires_shipping] => 1
-						[taxable] => 1
-						[fulfillment_service] => manual
-						[product_id] => 128436744
-						[variant_id] => 290813772
-					)
-
-			)
-
-		[currency] => AUD
-	)
-
-	//this is how you insert a rate
-	*/
-
 	$_RATES[] = array(
-		"service_name" => "International Shipping", //this is what the customer will see
-		"service_code" => "AUSPOST_INT", //can be anything you like
-		"total_price" => 10000, //in cents
-		"currency" => "AUD",
+		"service_name" => "USPS Priority Mail",
+		"service_code" => "USPS_PRIORITY_MAIL",
+		"total_price" => 10000,
+		"currency" => "USD"
 	);
 
 	return $_RATES;
--- test.php
+++ test.php
@@ -7,60 +7,14 @@ class TestCase extends \PHPUnit_Framework_TestCase
   public function testTest()
   {
     $data = array(
-        'origin' => array(
-          'country' => 'AU',
-          'postal_code' => '3000',
-          'province' => 'VIC',
-          'city' => 'melbourne',
-          'name' => '',
-          'address1' => '1 main street',
-          'address2' => '',
-          'address3' => '',
-          'phone' => '',
-          'fax' => '',
-          'address_type' => '',
-          'company_name' => '',
-          ),
         'destination' => array(
           'country' => 'AU',
           'postal_code' => '2000',
-          'province' => 'NSW',
-          'city' => '',
-          'name' => '',
-          'address1' => '',
-          'address2' => '',
-          'address3' => '',
-          'phone' => '',
-          'fax' => '',
-          'address_type' => '',
-          'company_name' => '',
           ),
         'items' => array(
             '0' => array(
-              'name' => 'product10',
-              'sku' => 'SKUP00310',
-              'quantity' => '1',
-              'grams' => '1000',
-              'price' => '300',
-              'vendor' => 'PRODUCT',
-              'requires_shipping' => '1',
-              'taxable' => '1',
-              'fulfillment_service' => 'manual',
-              'product_id' => '128436738',
-              'variant_id' => '290813760',
               ),
             '1' => array(
-              'name' => 'product11',
-              'sku' => 'SKUP0011',
-              'quantity' => '1',
-              'grams' => '1100',
-              'price' => '300',
-              'vendor' => 'PRODUCT',
-              'requires_shipping' => '1',
-              'taxable' => '1',
-              'fulfillment_service' => 'manual',
-              'product_id' => '128436744',
-              'variant_id' => '290813772',
               ),
             ),
             'currency' => 'AUD',
@@ -69,10 +23,10 @@ class TestCase extends \PHPUnit_Framework_TestCase
     $this->assertEquals(
         array(
           array(
-            'service_name' => 'International Shipping',
-            'service_code' => 'AUSPOST_INT',
+            'service_name' => 'USPS Priority Mail',
+            'service_code' => 'USPS_PRIORITY_MAIL',
             'total_price' => 10000,
-            'currency' => 'AUD'
+            'currency' => 'USD'
             )
           ), calculateshipping($data));
   }
