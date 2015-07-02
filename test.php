347b176edbf3787bb89f4531e2c07ae1bf7c6170
apeckham <apeckham@gmail.com>
2015-07-02 15:26:34 +0200

    reformat

--- test.php
+++ test.php
@@ -5,27 +5,24 @@ require './calculateshipping.php';
 class TestCase extends \PHPUnit_Framework_TestCase
 {
   public function testAustralia() {
-    $this->assertEquals(array(), calculateshipping(array(
-            'destination' => array('country' => 'AU')
-            )));
+    $this->assertEquals([], calculateshipping([
+          'destination' => ['country' => 'AU']
+          ]));
   }
 
   public function testUS()
   {
-    $data = array(
-        'destination' => array('country' => 'US', 'postal_code' => '2000'),
-        'items' => array('0' => array(), '1' => array()),
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '90210'],
+        'items' => ['0' => [], '1' => []],
         'currency' => 'AUD',
-        );
+        ]);
 
-    $this->assertEquals(
-        array(
-          array(
-            'service_name' => 'USPS Priority Mail',
-            'service_code' => 'USPS_PRIORITY_MAIL',
-            'total_price' => 10000,
-            'currency' => 'USD'
-            )
-          ), calculateshipping($data));
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS_PRIORITY_MAIL',
+        'total_price' => 10000,
+        'currency' => 'USD'
+        ]], $actual);
   }
 }
