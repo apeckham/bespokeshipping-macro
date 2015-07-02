e1ced815e5ba4c6066829505e76d9b7cf42ea16a
apeckham <apeckham@gmail.com>
2015-07-02 14:57:10 +0200

    reformat

--- calculateshipping.php
+++ calculateshipping.php
@@ -1,27 +1,27 @@
 <?php
 /* This macro will be parsed as PHP code (see http://www.php.net)
 
-The calculateshipping function is called every time a shipping calculation request is made by Shopify.
+   The calculateshipping function is called every time a shipping calculation request is made by Shopify.
 
-The function must return an array of available shipping options, otherwise no shipping options will be returned to your customers.
-*/
+   The function must return an array of available shipping options, otherwise no shipping options will be returned to your customers.
+ */
 function calculateshipping($DATA) {
 
-	/* do not edit above this line */
+  /* do not edit above this line */
 
-	$_RATES = array();
+  $_RATES = array();
 
-	if ($DATA['destination']['country'] == 'US') {
-		$_RATES[] = array(
-			"service_name" => "USPS Priority Mail",
-			"service_code" => "USPS_PRIORITY_MAIL",
-			"total_price" => 10000,
-			"currency" => "USD"
-		);
-	}
+  if ($DATA['destination']['country'] == 'US') {
+    $_RATES[] = array(
+        "service_name" => "USPS Priority Mail",
+        "service_code" => "USPS_PRIORITY_MAIL",
+        "total_price" => 10000,
+        "currency" => "USD"
+        );
+  }
 
-	return $_RATES;
+  return $_RATES;
 
-	/* do not edit below this line */
+  /* do not edit below this line */
 
 }
