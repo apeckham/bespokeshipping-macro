419a58a24a3eaac5e245c20b5b8733b1fe849d92
apeckham <apeckham@gmail.com>
2015-07-03 13:17:45 +0200

    add shipping prices for quantity of 2

--- test.php
+++ test.php
@@ -84,10 +84,131 @@ class TestCase extends \PHPUnit_Framework_TestCase
         ]], $actual);
   }
 
+  public function testZone2_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '13000'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-2',
+        'total_price' => 616,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
+  public function testZone3_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '44113'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-3',
+        'total_price' => 725,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
+  public function testZone4_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '28203'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-4',
+        'total_price' => 810,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
+  public function testZone5_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '32209'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-5',
+        'total_price' => 1066,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
+  public function testZone6_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '75201'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-6',
+        'total_price' => 1337,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
+  public function testZone7_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '77900'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-7',
+        'total_price' => 1628,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
+  public function testZone8_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '90001'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-8',
+        'total_price' => 1628,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
+  public function testZone1_twoItems()
+  {
+    $actual = calculateshipping([
+        'destination' => ['country' => 'US', 'postal_code' => '14607'],
+        'items' => [[], []]
+        ]);
+
+    $this->assertEquals([[
+        'service_name' => 'USPS Priority Mail',
+        'service_code' => 'USPS-ZONE-1',
+        'total_price' => 616,
+        'currency' => 'USD'
+        ]], $actual);
+  }
+
   public function testUnservedZip()
   {
     $actual = calculateshipping([
-        'destination' => ['country' => 'US', 'postal_code' => '96939']
+        'destination' => ['country' => 'US', 'postal_code' => '96939'],
+        'items' => []
         ]);
 
     $this->assertEquals([], $actual);
@@ -96,7 +217,8 @@ class TestCase extends \PHPUnit_Framework_TestCase
   public function testInvalidZip()
   {
     $actual = calculateshipping([
-        'destination' => ['country' => 'US', 'postal_code' => '00000']
+        'destination' => ['country' => 'US', 'postal_code' => '00000'],
+        'items' => []
         ]);
 
     $this->assertEquals([], $actual);
