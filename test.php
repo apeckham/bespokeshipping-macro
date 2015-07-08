c2c1c1c7fb5083747da8a34ff7c6b06c3dfc7fc3
apeckham <apeckham@gmail.com>
2015-07-08 21:19:13 +0100

    simplify specs

--- test.php
+++ test.php
@@ -3,58 +3,58 @@ require './calculateshipping.php';
 
 class TestCase extends \PHPUnit_Framework_TestCase {
   public function testZone8_90210() {
-    $this->assertShipping('USPS-ZONE-8', 997, '90210', 1);
+    $this->assertShipping('8', 997, '90210', 1);
   }
 
   public function testZone3_10001() {
-    $this->assertShipping('USPS-ZONE-3', 544, '10001', 1);
+    $this->assertShipping('3', 544, '10001', 1);
   }
 
   public function testZone8() {
-    $this->assertShipping('USPS-ZONE-8', 997, '96962', 1);
+    $this->assertShipping('8', 997, '96962', 1);
   }
 
   public function testZone3() {
-    $this->assertShipping('USPS-ZONE-3', 544, '44113', 1);
+    $this->assertShipping('3', 544, '44113', 1);
   }
 
   public function testZone1() {
-    $this->assertShipping('USPS-ZONE-1', 532, '14607', 1);
+    $this->assertShipping('1', 532, '14607', 1);
   }
 
   public function testZone2_twoItems() {
-    $this->assertShipping('USPS-ZONE-2', 616, '13000', 2);
+    $this->assertShipping('2', 616, '13000', 2);
   }
 
   public function testZone3_twoItems() {
-    $this->assertShipping('USPS-ZONE-3', 725, '44133', 2);
+    $this->assertShipping('3', 725, '44133', 2);
   }
 
   public function testZone4_twoItems() {
-    $this->assertShipping('USPS-ZONE-4', 810, '28203', 2);
+    $this->assertShipping('4', 810, '28203', 2);
   }
 
   public function testZone5_twoItems() {
-    $this->assertShipping('USPS-ZONE-5', 1066, '32209', 2);
+    $this->assertShipping('5', 1066, '32209', 2);
   }
 
   public function testZone6_twoItems() {
-    $this->assertShipping('USPS-ZONE-6', 1337, '75201', 2);
+    $this->assertShipping('6', 1337, '75201', 2);
   }
 
   public function testZone7_twoItems() {
-    $this->assertShipping('USPS-ZONE-7', 1628, '77900', 2);
+    $this->assertShipping('7', 1628, '77900', 2);
   }
 
   public function testZone8_twoItems() {
-    $this->assertShipping('USPS-ZONE-8', 1628, '90001', 2);
+    $this->assertShipping('8', 1628, '90001', 2);
   }
 
   public function testZone1_twoItems() {
-    $this->assertShipping('USPS-ZONE-1', 616, '14607', 2);
+    $this->assertShipping('1', 616, '14607', 2);
   }
   
-  public function test_threeItems() {
+  public function test_tooManyItems() {
     $actual = calculateshipping([
         'destination' => ['country' => 'US', 'postal_code' => 90210],
         'items' => array_fill(0, 3, [])
@@ -63,7 +63,7 @@ class TestCase extends \PHPUnit_Framework_TestCase {
     $this->assertEquals([], $actual);
   }
   
-  public function assertShipping($service_code, $total_price, $postal_code, $items_count) {
+  public function assertShipping($zone_number, $total_price, $postal_code, $items_count) {
     $actual = calculateshipping([
         'destination' => ['country' => 'US', 'postal_code' => $postal_code],
         'items' => array_fill(0, $items_count, [])
@@ -71,7 +71,7 @@ class TestCase extends \PHPUnit_Framework_TestCase {
 
     $this->assertEquals([[
         'service_name' => 'USPS Priority Mail',
-        'service_code' => $service_code,
+        'service_code' => 'USPS-ZONE-' . $zone_number,
         'total_price' => $total_price,
         'currency' => 'USD'
         ]], $actual);
