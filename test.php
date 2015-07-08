987cb70b52ad966d3cf31907880de4804c68d3fa
apeckham <apeckham@gmail.com>
2015-07-08 21:22:29 +0100

    refactor specs

--- test.php
+++ test.php
@@ -2,65 +2,44 @@
 require './calculateshipping.php';
 
 class TestCase extends \PHPUnit_Framework_TestCase {
-  public function testZone8_90210() {
-    $this->assertShipping('8', 997, '90210', 1);
-  }
-
-  public function testZone3_10001() {
+  public function testShippingOneItem() {
+    $this->assertShipping('1', 532, '14607', 1);
+    $this->assertShipping('3', 544, '44113', 1);
     $this->assertShipping('3', 544, '10001', 1);
-  }
-
-  public function testZone8() {
+    $this->assertShipping('8', 997, '90210', 1);
     $this->assertShipping('8', 997, '96962', 1);
   }
-
-  public function testZone3() {
-    $this->assertShipping('3', 544, '44113', 1);
-  }
-
-  public function testZone1() {
-    $this->assertShipping('1', 532, '14607', 1);
-  }
-
-  public function testZone2_twoItems() {
+  
+  public function testShippingTwoItems() {
+    $this->assertShipping('1', 616, '14607', 2);
     $this->assertShipping('2', 616, '13000', 2);
-  }
-
-  public function testZone3_twoItems() {
     $this->assertShipping('3', 725, '44133', 2);
-  }
-
-  public function testZone4_twoItems() {
     $this->assertShipping('4', 810, '28203', 2);
-  }
-
-  public function testZone5_twoItems() {
     $this->assertShipping('5', 1066, '32209', 2);
-  }
-
-  public function testZone6_twoItems() {
     $this->assertShipping('6', 1337, '75201', 2);
-  }
-
-  public function testZone7_twoItems() {
     $this->assertShipping('7', 1628, '77900', 2);
-  }
-
-  public function testZone8_twoItems() {
     $this->assertShipping('8', 1628, '90001', 2);
   }
-
-  public function testZone1_twoItems() {
-    $this->assertShipping('1', 616, '14607', 2);
-  }
   
-  public function test_tooManyItems() {
+  public function testTooManyItems() {
     $actual = calculateshipping([
         'destination' => ['country' => 'US', 'postal_code' => 90210],
         'items' => array_fill(0, 3, [])
         ]);
 
     $this->assertEquals([], $actual);
+  }
+
+  public function testUnservedZip() {
+    $this->assertNoShipping('US', '96939');
+  }
+
+  public function testInvalidZip() {
+    $this->assertNoShipping('US', '00000');
+  }
+
+  public function testAU() {
+    $this->assertNoShipping('AU', null);
   }
   
   public function assertShipping($zone_number, $total_price, $postal_code, $items_count) {
@@ -75,18 +54,6 @@ class TestCase extends \PHPUnit_Framework_TestCase {
         'total_price' => $total_price,
         'currency' => 'USD'
         ]], $actual);
-  }
-
-  public function testUnservedZip() {
-    $this->assertNoShipping('US', '96939');
-  }
-
-  public function testInvalidZip() {
-    $this->assertNoShipping('US', '00000');
-  }
-
-  public function testAU() {
-    $this->assertNoShipping('AU', null);
   }
   
   public function assertNoShipping($country, $postal_code) {
